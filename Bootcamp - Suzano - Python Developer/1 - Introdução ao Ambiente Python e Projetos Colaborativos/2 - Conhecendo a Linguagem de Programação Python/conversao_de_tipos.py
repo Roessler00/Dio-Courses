09ae86b395a147140e2b894a9675e6a836fa465c
João Roessler <56047991+Roessler00@users.noreply.github.com>
2025-09-04 05:11:36 -0300

    feat:tipos e input

---
+++ Bootcamp - Suzano - Python Developer/1 - Introdução ao Ambiente Python e Projetos Colaborativos/2 - Conhecendo a Linguagem de Programação Python/conversao_de_tipos.py
@@ -0,0 +1,35 @@
+preco = 10
+print(preco)
+
+# Int para Float
+preco = float(preco)
+print(preco)
+preco = 10 / 2
+print(preco)
+
+# Float para Int
+preco = 10.30
+print(preco)
+preco = int(preco)
+print(preco)
+
+# Conversão por Divisão
+preco = 10
+print(preco)
+print(preco / 2)
+print(preco // 2)
+
+# Numérico para String
+preco = 10.50
+idade = 25
+print(str(preco))
+print(str(idade))
+texto = f"idade {idade} preco {preco}"
+print(texto)
+
+# String para Numérico
+preco = "10.50"
+idade = "28"
+print(float(preco))
+print(float(idade))
+
